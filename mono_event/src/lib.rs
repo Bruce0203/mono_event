e1e34ccca0dbc385063a9f95f9d5ff906dcd7571
Bruce0203 <BruceFreedy@gmail.com>
2024-05-15 23:31:13 +0900

    version change to 0.2.0

--- mono_event/src/lib.rs
+++ mono_event/src/lib.rs
@@ -1,9 +1,6 @@
-#![feature(macro_metavar_expr)]
 use mono_event_derive::expand_listener_structs;
 
-pub use mono_event_derive::{
-    event, high_priority, highest_priority, listen, low_priority, lowest_priority, normal_priority,
-};
+pub use mono_event_derive::{event, listen};
 
 use std::io::Result;
 
